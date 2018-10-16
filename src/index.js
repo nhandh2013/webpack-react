a1d958f723d6fe65e7a1b95d81a36efe277e137c
nhan.do <nhan.do@niteco.se>
2018-10-16 17:53:12 +0700

    add react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
 import ReactDOM from 'react-dom';
 import App from './views/index.js';
+import {
+  BrowserRouter,
+  Route,
+  Link
+  } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 
 if(module.hot) module.hot.accept('./views/index.js', () => {
   ReactDOM.render(<App />, document.getElementById('root'));
